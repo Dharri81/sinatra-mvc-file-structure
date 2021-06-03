dfeead5dd4944d1cac2c12aaa888f6bdcc545497
David <dharri81@gmail.com>
2021-06-03 17:30:08 -0600

    commit tree 7dfc19dc5b3e131cc8835b3d108e1262b14238dc

---
+++ app/models/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+
+    attr_accessor :name, :breed, :age
+    attr_reader :id
+
+    @@all = []
+
+    def initialize (name, breed, age)
+
+        
+        @name = name
+        @breed = breed
+        @age = age
+        @@all << self
+
+    end
+
+    def self.all
+        @@all
+    end
+    
+
+end
+
